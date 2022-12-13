ae0292054c4e15bd4227400d92166c63c3878775
nts30 <alex0611f@gmail.com>
2022-12-13 21:36:27 +0300

    zcfdgfhf

--- src/components/organisms/MovieCard/MovieCard.js
+++ src/components/organisms/MovieCard/MovieCard.js
@@ -3,22 +3,20 @@ import "./movieCard.scss";
 
 export class MovieCard extends Component {
   static get observedAttributes() {
-    return ["title", "poster", "comments", "id", "rating"];
+    return ["title", "poster", "descriptions", "id", "rating"];
   }
 
   render() {
     return `
         <div class="movie">
             <div class="movie-image">
-            <span class="play"><span class="name">${this.props.title}</span></span>
-            <a href="#"><img src="../../assets/images/${this.props.poster}" alt="" /></a>
+              <span class="play"><span class="name">${this.props.title}</span></span>
+              <a href="#"><img src="../../assets/images/${this.props.poster}" alt="${this.props.title}"/></a>
             </div>
             <div class="rating">
-            <p>RATING</p>
-            <div class="stars">
-                <div class="stars-in"></div>
-            </div>
-            <span class="comments">${JSON.parse(this.props.comments)?.length ?? 0}</span>
+              <p>RATING</p>
+              <div class="stars">
+                  <div class="stars-in"></div>
             </div>
         </div>
         `;
--- src/services/DataBase.js
+++ src/services/DataBase.js
@@ -8,22 +8,11 @@ import {
   updateDoc,
   deleteDoc,
 } from "firebase/firestore";
-import { API_KEY } from "../../constants/envValues";
+import { cloudService } from "./Cloud";
 
 export class Database {
   constructor() {
-    const firebaseConfig = {
-      apiKey: process.env.API_KEY,
-      authDomain: "todo-2-6f8a5.firebaseapp.com",
-      projectId: "todo-2-6f8a5",
-      storageBucket: "todo-2-6f8a5.appspot.com",
-      messagingSenderId: "871943033561",
-      appId: "1:871943033561:web:5c2cc42b12df6413bf24c8",
-      measurementId: "G-30GY0GGDZV"
-    };
-
-    const app = initializeApp(firebaseConfig);
-    this._database = getFirestore(app);
+    this._database = getFirestore(cloudService.app);
   }
 
   create(collectionKey, body) {
@@ -56,3 +45,5 @@ export class Database {
     return Database.instance;
   }
 }
+
+export const databaseService = new Database();
